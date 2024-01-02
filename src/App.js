92e16262f521dfcd0c22ca060c434c8ee54eb4b5
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2024-01-03 04:47:34 +0530

    First Release

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+function extractYouTubeVideoId(url) {
+  // Regular expression pattern to match the YouTube video ID
+  var pattern = /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/ ]{11})/i;
+
+  // Execute the regular expression to get the video ID
+  var match = url.match(pattern);
+
+  // If there's a match, return the video ID; otherwise, return null
+  return (match && match[1]) ? match[1] : null;
+}
+
+
 function App() {
+  var [url,setUrl] = useState('');
+  var videoId,imgUrl;
+  var [img,setImg] = useState('');
+  var [toggleImage, setToggleImage] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Youtube Thumbnail Downloader</h1>
+      <input type="text" placeholder='Enter Youtube URL' value={url} onChange={(e)=>setUrl(e.target.value)} className='inputurl'/>
+      <button className='submitbtn' onClick={()=>{
+        videoId = extractYouTubeVideoId(url);
+        imgUrl = "https://img.youtube.com/vi/"+videoId+"/maxresdefault.jpg";
+        setImg(imgUrl);
+        setToggleImage(true);
+
+      }}>Get Thumbnail</button>
+      {toggleImage && <img src={img} alt='Thumbnail' />}
     </div>
   );
 }
